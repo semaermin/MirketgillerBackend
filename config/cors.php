3ff83ec695660762cfe4e5a31cc3857d23008e65
semaermin <sema.erminn@gmail.com>
2024-12-15 21:10:56 +0300

    update

--- config/cors.php
+++ config/cors.php
@@ -15,9 +15,9 @@ return [
     |
     */
 
-    // 'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
-    'paths' => ['https://www.mirketgiller.com.tr/*'],
+    // 'paths' => ['https://www.mirketgiller.com.tr/*'],
 
     'allowed_methods' => ['*'],
 
